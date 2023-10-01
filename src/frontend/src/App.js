1669b432fcacfd649f576506c5f03483a4fc5aa1
Jayden Kanbour <jkanbour1@gmail.com>
2023-10-01 23:46:36 +1100

    simplified ui

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
-import ChatScreen from 'src/pages/ChatScreen';
 import HomeScreen from 'src/pages/HomeScreen';
 
 const AppNavigator = createStackNavigator(
   {
     Home: HomeScreen,
-    Chat: ChatScreen,
   },
   {
     initialRouteName: 'Home',
